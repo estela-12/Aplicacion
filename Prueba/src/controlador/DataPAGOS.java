15ac1223fdf82455d0f99481e3b48aa0fbfb272a
Tupatron-uli <ulisesisraelhernandezgarcia@gmail.com>
2024-12-06 21:46:23 -0600

    acabe el codigo de pagos

--- Prueba/src/controlador/DataPAGOS.java
+++ Prueba/src/controlador/DataPAGOS.java
@@ -1,5 +1,140 @@
 package controlador;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+import javax.swing.JOptionPane;
+
+import modelo.Pagos;
+import modelo.Reporte;
+
 public class DataPAGOS {
+	Conxion co=new Conxion();
+
+	private Connection cone;
+	private PreparedStatement ps;
+
+	
+	public boolean guardar(Pagos pag) {
+		boolean guarda=false;
+		cone=co.conectar(); 
+		try {
+			ps=cone.prepareStatement("insert into pagos values(?,?,?,?)");
+			ps.setInt(1,pag.getIdPago());
+			ps.setInt(2,pag.getSaldoPendiente());
+			ps.setInt(3,pag.getSaldoApagar());
+			java.sql.Date sqlDate = new java.sql.Date(pag.getFechaLimiteaPagar().getTime());
+			ps.setDate(4, sqlDate);
+			int filasModificadas=ps.executeUpdate();
+			if(filasModificadas>0) {
+				guarda=true;
+				JOptionPane.showMessageDialog(null,"Pago realizado correctamente");
+				
+			}else {
+				JOptionPane.showMessageDialog(null,"Pago no realizado correctamente");
+
+				
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+			JOptionPane.showMessageDialog(null,"Error al guardar pago"+ e);
+
+		}
+		return guarda;
+	}
+	public Pagos consultarPago(int idPago) {
+		Pagos pag=new Pagos();
+		String sql="select * from pagos where idPagos=?";
+		cone=co.conectar();
+		LocalDate VistaPagos= null;
+		try {
+			ps=cone.prepareStatement(sql);
+			ps.setInt(1,idPago);
+			ResultSet rs=ps.executeQuery();
+			if(rs.next()) {
+				pag.setIdPago(rs.getInt(1));
+				pag.setSaldoPendiente(rs.getInt(2));
+				pag.setSaldoApagar(rs.getInt(3));
+				VistaPagos=rs.getDate(4).toLocalDate();
+				
+			}else {
+				JOptionPane.showMessageDialog(null,"No se encontro el pago");
+			}
+			ps.close();
+			cone.close();
+			rs.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+			JOptionPane.showMessageDialog(null,"Error al consultar pago"+e);
+
+			
+		}
+		return pag;
+	}
+	
+	public boolean actualizar(Pagos pag) {
+		boolean actuali=false;
+		cone=co.conectar();
+		try {
+			ps=cone.prepareStatement("update pagos set saldoPendiente=?,saldoApagar=?,fechaLimiteParaPagar=? where idPago=?");
+			ps.setInt(1,pag.getIdPago());
+			ps.setInt(2,pag.getSaldoPendiente());
+			ps.setInt(3,pag.getSaldoApagar());
+			java.sql.Date sqlDate = new java.sql.Date(pag.getFechaLimiteaPagar().getTime());
+			ps.setDate(4, sqlDate);
+			int filasModificadas=ps.executeUpdate();
+			if(filasModificadas>0) {
+				actuali=true;
+				JOptionPane.showMessageDialog(null,"Pago actualizado correctamente");
+				
+			}else {
+				JOptionPane.showMessageDialog(null,"Pago no actualizado");
+
+				
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+			JOptionPane.showMessageDialog(null,"Error al actualizar"+ e);
+
+		}
+		return actuali;
+		
+	}
+	public boolean eliminar(int idPago) {
+		boolean elimina=false;
+		cone=co.conectar();
+		try {
+			ps=cone.prepareStatement("update pagos set where idPago=?");
+			ps.setInt(1, idPago);
+			int filasModificadas=ps.executeUpdate();
+			if(filasModificadas>0) {
+				JOptionPane.showMessageDialog(null,"Pago Eliminado Correctamente");
+				
+			}else {
+				JOptionPane.showMessageDialog(null,"Pago no Eliminado");
+
+				
+			}
+			ps.close();
+			cone.close();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+			JOptionPane.showMessageDialog(null,"Error al eliminar"+ e);
+
+		}
+		return elimina;
+		
+	}
+	
 
 }
--- Prueba/src/modelo/Pagos.java
+++ Prueba/src/modelo/Pagos.java
@@ -1,5 +1,49 @@
 package modelo;
 
+import java.sql.Date;
+
 public class Pagos {
+	private int idPago;
+	private int saldoPendiente;
+	private int SaldoApagar;
+	private Date fechaLimiteaPagar;
+	public int getIdPago() {
+		return idPago;
+	}
+	public void setIdPago(int idPago) {
+		this.idPago = idPago;
+	}
+	public int getSaldoPendiente() {
+		return saldoPendiente;
+	}
+	public void setSaldoPendiente(int saldoPendiente) {
+		this.saldoPendiente = saldoPendiente;
+	}
+	public int getSaldoApagar() {
+		return SaldoApagar;
+	}
+	public void setSaldoApagar(int saldoApagar) {
+		SaldoApagar = saldoApagar;
+	}
+	public Date getFechaLimiteaPagar() {
+		return fechaLimiteaPagar;
+	}
+	public void setFechaLimiteaPagar(Date fechaLimiteaPagar) {
+		this.fechaLimiteaPagar = fechaLimiteaPagar;
+	}
+	public Pagos(int idPago, int saldoPendiente, int saldoApagar, Date fechaLimiteaPagar) {
+		super();
+		this.idPago = idPago;
+		this.saldoPendiente = saldoPendiente;
+		SaldoApagar = saldoApagar;
+		this.fechaLimiteaPagar = fechaLimiteaPagar;
+	}
+	public Pagos() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
 
 }
